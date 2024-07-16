afb869d012c12061301c7a46f100cc4175f04e7e
matimayor <matias.mayor99@gmail.com>
2024-07-16 17:19:14 -0300

    Correcciones calculo code average

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
+// jest.config.js
 
 module.exports = {
-  coverageDirectory: 'coverage', 
-  collectCoverage: true, 
-  collectCoverageFrom: [
-    'src/**/*.js', 
+  coverageDirectory: 'coverage', // Directorio donde se guardará el informe de cobertura
+  collectCoverage: true, // Activar la recolección de cobertura
+  coverageReporters: [
+    'text-summary', // Mostrar resumen de cobertura como texto
+    'html', 
   ],
  
 };
