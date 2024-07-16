2814b8fbd349e3d4a24c195f5370a9d49eea2a03
matimayor <matias.mayor99@gmail.com>
2024-07-16 17:11:01 -0300

    Agrego calculo de Code Average

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
-// jest.config.js
+
 module.exports = {
-  testEnvironment: 'node'
+  coverageDirectory: 'coverage', 
+  collectCoverage: true, 
+  collectCoverageFrom: [
+    'src/**/*.js', 
+  ],
+ 
 };
