4c720730c1b3f6461ca92bc2dd58da85459daaeb
matimayor <matias.mayor99@gmail.com>
2024-07-26 09:32:31 -0300

    Agrego integracion con sonarqube

--- ahorcado.test.js
+++ ahorcado.test.js
@@ -1,26 +1,35 @@
 const Ahorcado = require('./ahorcado');
 
-test('debería iniciar el juego con una palabra oculta de un conjunto de palabras', () => {
+test('Debería permitir ingresar una letra válida', () => {
+  const palabras = ['desarrollo'];
+  const juego = new Ahorcado(palabras);
+  juego.adivinar('d');
+  expect(juego.getEstado()).toBe('d_________');
+  expect(juego.getLetrasIncorrectas()).toEqual([]);
+  expect(juego.intentos).toBe(0);
+});
+
+test('Debería iniciar el juego con una palabra oculta de un conjunto de palabras', () => {
   const palabras = ['javascript', 'programacion', 'desarrollo'];
   const juego = new Ahorcado(palabras);
   expect(juego.getEstado()).toBe('_'.repeat(juego.palabra.length));
 });
 
-test('debería permitir al jugador adivinar letras', () => {
+test('Debería permitir al jugador adivinar letras', () => {
   const palabras = ['javascript'];
   const juego = new Ahorcado(palabras);
   juego.adivinar('j');
   expect(juego.getEstado()).toBe('j_________');
 });
 
-test('debería llevar un registro de las letras adivinadas incorrectamente', () => {
+test('Debería llevar un registro de las letras adivinadas incorrectamente', () => {
   const palabras = ['javascript'];
   const juego = new Ahorcado(palabras);
   juego.adivinar('x');
   expect(juego.getLetrasIncorrectas()).toEqual(['x']);
 });
 
-test('debería indicar si el jugador ha ganado', () => {
+test('Debería indicar si el jugador ha ganado', () => {
   const palabras = ['javascript'];
   const juego = new Ahorcado(palabras);
   juego.adivinar('j');
@@ -35,7 +44,7 @@ test('debería indicar si el jugador ha ganado', () => {
   expect(juego.esGanador()).toBe(true);
 });
 
-test('debería indicar si el jugador ha perdido', () => {
+test('Debería indicar si el jugador ha perdido', () => {
   const palabras = ['javascript'];
   const juego = new Ahorcado(palabras);
   juego.adivinar('x');
